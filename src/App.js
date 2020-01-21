4106c90d7186b87799af927245ea2929971b7d79
Ernur Kurmanbekov <erazavr85@gmail.com>
2020-01-21 18:46:37 +0600

    Wrote styles for infoPage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Layout from "./components/Layout/Layout";
 import {Route, Switch} from "react-router";
 import HomePage from "./containers/HomePage/HomePage";
-import InformPage from "./containers/InformPage/InformPage";
+import InfoPage from "./containers/InfoPage/InfoPage";
 
 const App = () => (
     <Layout>
       <Switch>
         <Route path='/' exact component={HomePage}/>
-        <Route path='/shows/:id' exact component={InformPage}/>
+        <Route path='/shows/:id' exact component={InfoPage}/>
       </Switch>
     </Layout>
 );
