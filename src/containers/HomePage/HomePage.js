65dc0a98889c8c074cebb0559ae6303a056c4434
Ernur Kurmanbekov <erazavr85@gmail.com>
2020-01-21 15:42:00 +0600

    complete homework-70

---
+++ src/containers/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Request from "../../components/Request/Request";
+const HomePage = () => {
+    return (
+        <Request/>
+    );
+};
+
+export default HomePage;
