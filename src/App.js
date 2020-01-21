5a6f974f146453bd3b06a9efc150d21a751c317e
Ernur Kurmanbekov <erazavr85@gmail.com>
2020-01-21 15:42:31 +0600

    completed homework-70

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./components/Layout/Layout";
+import {Route, Switch} from "react-router";
+import HomePage from "./containers/HomePage/HomePage";
+import InformPage from "./containers/InformPage/InformPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <Layout>
+      <Switch>
+        <Route path='/' exact component={HomePage}/>
+        <Route path='/shows/:id' exact component={InformPage}/>
+      </Switch>
+    </Layout>
+);
 
 export default App;
